61efff9404a92748d7af2b609eafe290ec130fda
L0u1Za <nigoluiza@mail.ru>
2022-05-11 19:10:20 +0300

    Added simple prefix search.

--- client/GameAggregator/app/src/main/java/com/example/gameaggregator/Search.java
+++ client/GameAggregator/app/src/main/java/com/example/gameaggregator/Search.java
@@ -1,10 +1,17 @@
 package com.example.gameaggregator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Search {
-    static public void find(String request) {
-
-    }
-    static public void clear() {
-
+    static public List<Game> find(String request) {
+        List<Game> result = new ArrayList<Game>();
+        for (int i = 0; i < Data.GAMES.size(); ++i) {
+            Game game = Data.GAMES.get(i);
+            if (game.getName().startsWith(request)) {
+                result.add(game);
+            }
+        }
+        return result;
     }
 }
